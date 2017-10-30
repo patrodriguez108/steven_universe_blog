0f60acb9a04b4492af5b1bf3239e9ce7eac8e5b4
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2017-10-29 20:29:27 -0500

    Add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
 
   get '/about', to: 'pages#about'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get 'signup' => 'users#new'
   post '/users' => 'users#create'
 end
