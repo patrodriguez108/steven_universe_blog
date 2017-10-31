7f877938e9ae7bf213b57714c8e2ce6eea1d7dec
Patrick Rodriguez <pattyroddy@Patrick-Rodriguezs-MacBook-Pro.local>
2017-10-31 07:34:35 -0500

    Change resources to standard routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
 
-  resources :posts do
-    resources :comments
-  end
+  # resources :posts do
+  #   resources :comments
+  # end
+
+  get '/posts', to: 'posts#index'
+  get '/posts/new', to: 'posts#new', as: 'new_post'
+  post '/posts', to: 'posts#create'
+  get '/posts/:id', to: 'posts#show', as: 'post'
+  put '/posts/:id', to: 'posts#update', as: 'edit_post'
+  delete '/posts/:id', to: 'posts#destroy'
+  
+  get '/posts/:post_id/comments', to: 'comments#index', as: 'post_comments'
+  get '/posts/:post_id/comments/new', to: 'comments#new'
+  post '/posts/:post_id/comments', to: 'comments#create'
+
 
   root "posts#index"
 
